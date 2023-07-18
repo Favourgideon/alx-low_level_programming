3921fb1a74b6e5764df8b08d48e7ab6dc7cb6cbc
Favourgideon <esthergideon111@gmail.com>
2023-07-18 15:07:52 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Descriptio: 'prints -putchar to stdout'
+ * Return@ alwaus 0
+ */
+int main(void)
+{
+	-putchar('_');
+        -putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('£');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+	return (0);
+}
