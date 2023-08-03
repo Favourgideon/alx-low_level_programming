7c47fb6f6c97055269f5a4dcff06c1e046ebabbe
Favourgideon <esthergideon111@gmail.com>
2023-08-02 22:40:42 -0700

    commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,16 +1,15 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
+
 
 /**
- * set_string - sets the value of a pointer to a char
- *
- * @s: double pointer
- * @to: pointer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* set_string - sets the value of a pointer to a char
+* @s: pointer to pointer
+* @to: pointer char
+*/
+
+
 void set_string(char **s, char *to)
 {
+
 	*s = to;
 }
