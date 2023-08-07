677fa298fddcd0ea1b43c6c8e523a31791d32067
Favourgideon <esthergideon111@gmail.com>
2023-08-07 03:25:28 -0700

    files

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,13 +1,15 @@
-#ifndef HOLBERTON_H_
-#define HOLBERTON_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int check_number(char *str);
+unsigned int string_length(char *str);
+void print_string(char *str);
+void *_calloc(unsigned int number, unsigned int size);
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
 
 #endif
