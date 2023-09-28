d6fad8a9c4181d318c097f4026439a7307c1d649
Favourgideon <esthergideo111@gmail.com>
2023-09-28 14:34:01 -0700

    Write a function that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void print_binary(unsigned long int n) {
+    if (n == 0) {
+        putchar('0');
+        return;
+    }
+
+    int bit_count = sizeof(unsigned long int) * 8;
+    int leading_zeros = 1; // Flag to skip leading zeros
+
+    for (int i = bit_count - 1; i >= 0; i--) {
+        unsigned long int mask = 1UL << i;
+        if ((n & mask) != 0) {
+            leading_zeros = 0;
+            putchar('1');
+        } else if (!leading_zeros) {
+            putchar('0');
+        }
+    }
+}
+
+int main() {
+    unsigned long int num = 42; // Change this to your input number
+    printf("Binary representation of %lu is: ", num);
+    print_binary(num);
+    putchar('\n');
+    return 0;
+}
