3ae261773789bf57b180f5bd5e47d82f54b50280
Favourgideon <esthergideo111@gmail.com>
2023-09-28 14:37:41 -0700

    Write a function that returns the value of a bit at a given index.

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int get_bit(unsigned long int n, unsigned int index) {
+    if (index >= sizeof(unsigned long int) * 8) {
+        return -1; // Index out of range
+    }
+
+    unsigned long int mask = 1UL << index;
+    return (n & mask) ? 1 : 0;
+}
+
+int main() {
+    unsigned long int num = 42; // Change this to your input number
+    unsigned int bit_index = 3; // Change this to the desired bit index
+    int bit_value = get_bit(num, bit_index);
+
+    if (bit_value != -1) {
+        printf("Bit %u of %lu is: %d\n", bit_index, num, bit_value);
+    } else {
+        printf("Error: Bit index out of range.\n");
+    }
+
+    return 0;
+}
